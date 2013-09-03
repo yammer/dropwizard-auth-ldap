8e09e15371b127ad530fd89c786259b780e83374
Peter Shaw <pshaw@yammer-inc.com>
2013-09-03 13:10:47 -0700

    Move ResourceAuthenticator to here from Clank

---
+++ src/main/java/com/yammer/dropwizard/authenticator/resources/ResourceAuthenticator.java
@@ -0,0 +1,25 @@
+package com.yammer.dropwizard.authenticator.resources;
+
+import com.google.common.base.Optional;
+import com.yammer.dropwizard.auth.AuthenticationException;
+import com.yammer.dropwizard.auth.Authenticator;
+import com.yammer.dropwizard.auth.basic.BasicCredentials;
+import com.yammer.dropwizard.authenticator.LdapAuthenticator;
+
+public class ResourceAuthenticator implements Authenticator<BasicCredentials, BasicCredentials> {
+
+    private final LdapAuthenticator ldapAuthenticator;
+
+    public ResourceAuthenticator(LdapAuthenticator ldapAuthenticator) {
+        this.ldapAuthenticator = ldapAuthenticator;
+    }
+
+    @Override
+    public Optional<BasicCredentials> authenticate(BasicCredentials credentials) throws AuthenticationException {
+        if (ldapAuthenticator.authenticate(credentials)) {
+            return Optional.of(credentials);
+        } else {
+            return Optional.absent();
+        }
+    }
+}
