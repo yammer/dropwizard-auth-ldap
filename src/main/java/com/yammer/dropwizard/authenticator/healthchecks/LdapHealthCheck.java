17026012b577494e2061296cac58dc75ec288512
cgray <cgray@yammer-inc.com>
2013-10-11 15:57:53 -0700

    LdapHealthcheck needs to use an Authenticator

--- src/main/java/com/yammer/dropwizard/authenticator/healthchecks/LdapHealthCheck.java
+++ src/main/java/com/yammer/dropwizard/authenticator/healthchecks/LdapHealthCheck.java
@@ -1,23 +1,23 @@
 package com.yammer.dropwizard.authenticator.healthchecks;
 
 import com.yammer.dropwizard.auth.AuthenticationException;
+import com.yammer.dropwizard.auth.Authenticator;
 import com.yammer.dropwizard.auth.basic.BasicCredentials;
-import com.yammer.dropwizard.authenticator.LdapAuthenticator;
 import com.yammer.metrics.core.HealthCheck;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
 public class LdapHealthCheck extends HealthCheck {
-    private final LdapAuthenticator ldapAuthenticator;
+    private final Authenticator<BasicCredentials, BasicCredentials> ldapAuthenticator;
 
-    public LdapHealthCheck(LdapAuthenticator ldapAuthenticator) {
+    public LdapHealthCheck(Authenticator<BasicCredentials, BasicCredentials> ldapAuthenticator) {
         super("ldap");
         this.ldapAuthenticator = checkNotNull(ldapAuthenticator, "ldapAuthenticator cannot be null");
     }
 
     @Override
     public Result check() throws AuthenticationException {
-        if (ldapAuthenticator.authenticate(new BasicCredentials("", ""))) {
+        if (ldapAuthenticator.authenticate(new BasicCredentials("", "")).isPresent()) {
             return Result.healthy();
         } else {
             return Result.unhealthy("Cannot contact authentication service");
