d23e8bdc3470381ccbc32dcbb74bc5bd6e6b14a8
Jamie Furness <jfurness@yammer-inc.com>
2014-01-22 14:18:48 +0000

    Adding support for restricting users to specific groups

--- src/main/java/com/yammer/dropwizard/authenticator/LdapAuthenticator.java
+++ src/main/java/com/yammer/dropwizard/authenticator/LdapAuthenticator.java
@@ -9,15 +9,25 @@ import org.slf4j.LoggerFactory;
 
 import javax.naming.AuthenticationException;
 import javax.naming.Context;
+import javax.naming.NamingEnumeration;
 import javax.naming.NamingException;
 import javax.naming.directory.InitialDirContext;
+import javax.naming.directory.SearchControls;
+import javax.naming.directory.SearchResult;
 import java.util.Hashtable;
+import java.util.Set;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
 public class LdapAuthenticator {
+
     private static final Logger LOG = LoggerFactory.getLogger(LdapAuthenticator.class);
     private static final Timer LDAP_AUTHENTICATION_TIMER = Metrics.defaultRegistry().newTimer(LdapAuthenticator.class, "authenticate");
+
+    private static String sanitizeEntity(String name) {
+        return name.replaceAll("[^A-Za-z0-9-_.]", "");
+    }
+
     protected final LdapConfiguration configuration;
 
     public LdapAuthenticator(LdapConfiguration configuration) {
@@ -34,17 +44,49 @@ public class LdapAuthenticator {
         return false;
     }
 
+    private boolean filterByGroup(InitialDirContext context, String sanitizedUsername) throws NamingException {
+        final Set<String> restrictedToGroups = configuration.getRestrictToGroups();
+        if (restrictedToGroups.isEmpty()) {
+            return true;
+        }
+
+        final StringBuilder groupFilter = new StringBuilder();
+        for (String group : restrictedToGroups) {
+            final String sanitizedGroup = sanitizeEntity(group);
+            groupFilter.append(String.format("(cn=%s)", sanitizedGroup));
+        }
+
+        final String filter = String.format("(&(memberUid=%s)(|%s))", sanitizedUsername, groupFilter.toString());
+
+        final String groupDN = String.format("ou=groups,%s", configuration.getSecurityPrincipal());
+        final NamingEnumeration<SearchResult> result = context.search(groupDN, filter, new SearchControls());
+        try {
+            return result.hasMore();
+        }
+        finally {
+            result.close();
+        }
+    }
+
     public boolean authenticate(BasicCredentials basicCredentials) throws com.yammer.dropwizard.auth.AuthenticationException {
         final TimerContext ldapAuthenticationTimer = LDAP_AUTHENTICATION_TIMER.time();
         try {
-            final String sanitizedUsername = sanitizeUsername(basicCredentials.getUsername());
+            final String sanitizedUsername = sanitizeEntity(basicCredentials.getUsername());
+            final String userDN = String.format("cn=%s,ou=people,%s", sanitizedUsername, configuration.getSecurityPrincipal());
+
             final Hashtable<String, String> env = contextConfiguration();
-            env.put(Context.SECURITY_PRINCIPAL, String.format(configuration.getSecurityPrincipal(), sanitizedUsername));
+
+            env.put(Context.SECURITY_PRINCIPAL, userDN);
             env.put(Context.SECURITY_CREDENTIALS, basicCredentials.getPassword());
 
             try {
-                new InitialDirContext(env).close();
-                return true;
+                final InitialDirContext context = new InitialDirContext(env);
+                try {
+                    return filterByGroup(context, sanitizedUsername);
+                }
+                finally {
+                    context.close();
+                }
             } catch (AuthenticationException ae) {
                 LOG.debug("{} failed to authenticate. {}", sanitizedUsername, ae);
             } catch (NamingException err) {
@@ -59,15 +101,13 @@ public class LdapAuthenticator {
 
     private Hashtable<String, String> contextConfiguration() {
         final Hashtable<String, String> env = new Hashtable<>();
+
         env.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");
         env.put(Context.PROVIDER_URL, configuration.getUri().toString());
         env.put("com.sun.jndi.ldap.connect.timeout", String.valueOf(configuration.getConnectTimeout().toMilliseconds()));
         env.put("com.sun.jndi.ldap.read.timeout", String.valueOf(configuration.getReadTimeout().toMilliseconds()));
         env.put("com.sun.jndi.ldap.connect.pool", "true");
-        return env;
-    }
 
-    private static String sanitizeUsername(String username) {
-        return username.replaceAll("[^A-Za-z0-9-_.]", "");
+        return env;
     }
 }
--- src/main/java/com/yammer/dropwizard/authenticator/LdapConfiguration.java
+++ src/main/java/com/yammer/dropwizard/authenticator/LdapConfiguration.java
@@ -1,63 +1,91 @@
 package com.yammer.dropwizard.authenticator;
 
 import com.google.common.cache.CacheBuilderSpec;
+import com.google.common.collect.Sets;
 import com.yammer.dropwizard.util.Duration;
 import org.hibernate.validator.constraints.NotEmpty;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 import java.net.URI;
+import java.util.Set;
 
 public class LdapConfiguration {
     @NotNull
     @Valid
     private URI uri = URI.create("ldaps://www.example.com:636");
-    @NotNull @Valid
+
+    @NotNull
+    @Valid
     private CacheBuilderSpec cachePolicy = CacheBuilderSpec.disableCaching();
-    @NotNull @NotEmpty
-    private String securityPrincipal = "cn=%s";
-    @NotNull @Valid
+
+    @NotNull
+    @NotEmpty
+    private String securityPrincipal = "dc=com";
+
+    @NotNull
+    @Valid
     private Duration connectTimeout = Duration.milliseconds(500);
-    @NotNull @Valid
+
+    @NotNull
+    @Valid
     private Duration readTimeout = Duration.milliseconds(500);
+
+    @NotNull
+    @Valid
+    private Set<String> restrictToGroups = Sets.newHashSet();
 
     public URI getUri() {
         return uri;
     }
 
-    public void setUri(URI uri) {
+    public LdapConfiguration setUri(URI uri) {
         this.uri = uri;
+        return this;
     }
 
     public CacheBuilderSpec getCachePolicy() {
         return cachePolicy;
     }
 
-    public void setCachePolicy(CacheBuilderSpec cachePolicy) {
+    public LdapConfiguration setCachePolicy(CacheBuilderSpec cachePolicy) {
         this.cachePolicy = cachePolicy;
+        return this;
     }
 
     public String getSecurityPrincipal() {
         return securityPrincipal;
     }
 
-    public void setSecurityPrincipal(String securityPrincipal) {
+    public LdapConfiguration setSecurityPrincipal(String securityPrincipal) {
         this.securityPrincipal = securityPrincipal;
+        return this;
     }
 
     public Duration getConnectTimeout() {
         return connectTimeout;
     }
 
-    public void setConnectTimeout(Duration connectTimeout) {
+    public LdapConfiguration setConnectTimeout(Duration connectTimeout) {
         this.connectTimeout = connectTimeout;
+        return this;
     }
 
     public Duration getReadTimeout() {
         return readTimeout;
     }
 
-    public void setReadTimeout(Duration readTimeout) {
+    public LdapConfiguration setReadTimeout(Duration readTimeout) {
         this.readTimeout = readTimeout;
+        return this;
+    }
+
+    public Set<String> getRestrictToGroups() {
+        return restrictToGroups;
+    }
+
+    public LdapConfiguration addRestrictedGroup(String group) {
+        restrictToGroups.add(group);
+        return this;
     }
 }
