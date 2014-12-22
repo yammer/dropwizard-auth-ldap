6c9b853cfbf9afb4b33fe2de37935514861b607e
Dariusz Lorenc <dariusz.lorenc@gmail.com>
2014-12-22 11:13:31 +1100

    reverting the last change - those exceptions seem to be there for a different reason

--- src/test/java/com/yammer/dropwizard/authenticator/tests/LdapAuthenticatorTest.java
+++ src/test/java/com/yammer/dropwizard/authenticator/tests/LdapAuthenticatorTest.java
@@ -1,13 +1,14 @@
 package com.yammer.dropwizard.authenticator.tests;
 
+import com.yammer.dropwizard.authenticator.LdapAuthenticator;
+import com.yammer.dropwizard.authenticator.LdapConfiguration;
 import io.dropwizard.auth.AuthenticationException;
 import io.dropwizard.auth.basic.BasicCredentials;
-
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import com.yammer.dropwizard.authenticator.LdapAuthenticator;
-import com.yammer.dropwizard.authenticator.LdapConfiguration;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 
 public class LdapAuthenticatorTest {
     private static LdapAuthenticator ldapAuthenticator;
@@ -20,21 +21,21 @@ public class LdapAuthenticatorTest {
 
     @Test(expected = AuthenticationException.class)
     public void badUser() throws AuthenticationException {
-        ldapAuthenticator.authenticate(new BasicCredentials("user", "password"));
+        assertThat(ldapAuthenticator.authenticate(new BasicCredentials("user", "password")), is(false));
     }
 
     @Test(expected = AuthenticationException.class)
     public void noPassword() throws AuthenticationException {
-        ldapAuthenticator.authenticate(new BasicCredentials("user", ""));
+        assertThat(ldapAuthenticator.authenticate(new BasicCredentials("user", "")), is(false));
     }
 
     @Test(expected = AuthenticationException.class)
     public void noUser() throws AuthenticationException {
-        ldapAuthenticator.authenticate(new BasicCredentials("", "password"));
+        assertThat(ldapAuthenticator.authenticate(new BasicCredentials("", "password")), is(false));
     }
 
     @Test(expected = AuthenticationException.class)
     public void badServer() throws AuthenticationException {
-        ldapAuthenticator.authenticate(new BasicCredentials("user", "password"));
+        assertThat(ldapAuthenticator.authenticate(new BasicCredentials("user", "password")), is(false));
     }
 }
